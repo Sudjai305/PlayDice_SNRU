d595ce6f7f8a2f968ca92cdaf7e1aeeacd43fc52
nam <sudjai.sa59@snru.ac.th>
2019-03-19 14:48:50 +0700

    Proiect succes

--- PlayDice/ViewController.swift
+++ PlayDice/ViewController.swift
@@ -9,15 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Explicit
+    var sourceDices = ["Dice1","Dice2","Dice3","Dice4","Dice5","Dice6"]
+    
+    
+    
 
     @IBOutlet weak var firstDicelmageView: UIImageView!
-    
-    
     @IBOutlet weak var secondDiceImageView: UIImageView!
-    
-    
-    
-    @IBOutlet weak var thirdDiceImageView: NSLayoutConstraint!
+    @IBOutlet weak var thirdDiceImageView: UIImageView!
     
     
     override func viewDidLoad() {
@@ -29,6 +30,20 @@ class ViewController: UIViewController {
     @IBAction func ramdomDiceButtom(_ sender: Any) {
         
         print("You Click Ramdom")
+        
+        //  for Dice1
+        let indexDice1: Int = Int.random(in: 0 ... 5)
+        firstDicelmageView.image=UIImage(named: sourceDices[indexDice1])
+        
+    //  for Dice2
+        let indexDice2: Int = Int.random(in: 0 ... 5)
+        secondDiceImageView.image=UIImage(named: sourceDices[indexDice2])
+        
+        //  for Dice3
+        let indexDice3: Int = Int.random(in: 0 ... 5)
+        thirdDiceImageView.image=UIImage(named: sourceDices[indexDice3])
+        
+
         
     }   //ramdomDice
     
